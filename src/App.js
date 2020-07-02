dcc0e21865f8bcb628f589f9910d0cbdb297265b
rahuljha661 <rahuljha12527@gmail.com>
2020-07-03 01:07:31 +0530

    adding material -ui

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import {fetchData} from './api';
 
 class App extends React.Component{
 
-
+  state={
+     data:  {},
+  }
    async componentDidMount(){
-        const data=await fetchData();
+        const fetchedData=await fetchData();
 
-        console.log(data);
+      this.setState({data:fetchedData})
     }
     render(){
+
+      const {data}=this.state;
+
+
         return (
             <div className={styles.container}>
-                <Cards />
+                <Cards data={data} />
                 <CountryPicker />
                 <Chart />
                 
