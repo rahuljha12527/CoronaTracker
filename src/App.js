4eef6fc78dca818e727e4d27851ee3e7188c055e
rahuljha661 <rahuljha12527@gmail.com>
2020-07-04 14:04:40 +0530

    complete corona tracker-app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {Cards,Chart,CountryPicker} from './components';
 import styles from './App.module.css';
 import {fetchData} from './api';
 
+import coronaImage from './images/covidn.jpg';
+
 class App extends React.Component{
 
   state={
@@ -38,6 +40,7 @@ class App extends React.Component{
 
         return (  
             <div className={styles.container}>
+              <img className={styles.image}  src={coronaImage} alt="COVID-19  "/>
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
                 <Chart data={data} country={country}/>
